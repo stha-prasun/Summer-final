25e6db6535dfd05434b9e596579ce47a43fde8f2
Prasun Shrestha <shresthaprasun88@gmail.com>
2026-07-26 13:37:49 +0545

    feat(cart): added cart in redux

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adminSlice from "./adminSlice.js";
+import cartReducer from "./cartSlice.js";
 
 const store = configureStore({
   reducer: {
     Admin: adminSlice,
+    Cart: cartReducer,
   },
 });
 
