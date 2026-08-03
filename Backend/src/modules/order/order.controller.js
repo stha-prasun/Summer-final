fd89fba3ab4b1ec336109262569466dd942a01fd
Sanrose <sanrosebhetuwal@gmail.com>
2026-08-03 16:34:04 +0545

    chore(order): backend controller and service created

---
+++ Backend/src/modules/order/order.controller.js
@@ -0,0 +1,53 @@
+import * as orderService from './order.service.js';
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const {orders} = req.query;
+    const orders = await orderService.getAllOrders(orders);
+    if (orders.length === 0) {
+      return res.status(200).json({
+        success: false,
+        message: 'No orders found.',
+        orders: [],
+      });
+    }
+    return res.status(200).json({
+        success: true,
+        orders,
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      success: false,
+      message: error.message || 'Failed to get orders.',
+    });
+  }
+};
+
+export const getOrderById = async (req, res) => {
+  try{
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: 'Order ID is required.',
+      });
+    }
+
+    const orderItem = await orderService.getOrderById(id);
+    if (!orderItem) {
+      return res.status(404).json({
+        success: false,
+        message: 'Order not found.',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      order: orderItem,
+    });
+  }catch(error){
+    console.log(error);
+
+  }
+}
